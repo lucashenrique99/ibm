eadb869d6adbe9914ee6debe89b16c72fb404bfa
Lucas <lucashenriques.dev@gmail.com>
2019-11-15 18:39:23 -0300

    Add new social midias links

--- ibm-ui/src/app/components/carousel/carousel.component.ts
+++ ibm-ui/src/app/components/carousel/carousel.component.ts
@@ -10,10 +10,9 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 export class CarouselComponent implements OnInit {
 
   @Input() captionMessage = "Igreja Batista Missionária";
-  @Input() showNavigationArrows = true;
-  showNavigationIndicators = true;
-  images = [1, 2, 3].map(() => `https://picsum.photos/1920/500?random&t=${Math.random()}`);
-  // images = ['https://scontent.fbsb9-1.fna.fbcdn.net/v/t1.0-0/cp0/e15/q65/p320x320/71043135_2406102292935346_5281979032915673088_o.jpg?_nc_cat=109&efg=eyJpIjoidCJ9&_nc_oc=AQkfWUtcQT3AKPuLIMqozYnuqpn7PjtDsJmgYLIDwVgE_apqNuM0Ywxexwu6uymlIjI&_nc_ht=scontent.fbsb9-1.fna&oh=2bcb9d1710486259183cf2b9d88c6da2&oe=5E3FBA13']
+  @Input() showNavigationArrows = false;
+  showNavigationIndicators = false;
+  images: Array<string>;
 
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
@@ -22,6 +21,8 @@ export class CarouselComponent implements OnInit {
   }
 
   ngOnInit() {
+    // this.images = [1, 2, 3].map(() => `https://picsum.photos/1920/500?random&t=${Math.random()}`);
+    this.images = ['assets/img/banner.webp'];
   }
 
 }
